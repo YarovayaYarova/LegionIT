13522d4c1511854ed4374c93b56eb9899d243668
Anna Yarova <yarovaya.yarova@gmail.com>
2022-12-18 22:39:11 +0800

    Initial commit

---
+++ src/main/java/lesson1/Lesson1Main.java
@@ -0,0 +1,29 @@
+package lesson1;
+
+import com.sun.jdi.connect.Connector;
+
+public class Lesson1Main {
+    public static void main(String[] args) {
+
+        String txt = "Hello World!!!";
+        int num = 5;
+        double doubleNum = 5.5;
+        long longNum = 123456789;
+        boolean booleanVar = true;
+        boolean booleanVar2 = false;
+        int a = 100;
+        int b = 200;
+        double result = (double) a / b;
+
+        System.out.println("Result is " + result);
+
+//        System.out.println(txt);
+//        System.out.println("number = " + num);
+//        System.out.println("double number = " + doubleNum);
+//        System.out.println("long number = " + longNum);
+//
+//        System.out.println("booleanVar = " + booleanVar);
+//        System.out.println("booleanVar2 = " + booleanVar2);
+
+    }
+}
